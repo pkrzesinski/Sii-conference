c9f25f2e485758c39d572ca005dbad8b0f10afb8
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-26 19:18:16 +0200

    Initial commit

---
+++ src/main/java/com/project/siiproject/SiiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.siiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiprojectApplication.class, args);
+	}
+
+}
