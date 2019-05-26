6e8f1d5a9eacc62ff9a306d089edbe606234ae58
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-26 21:55:17 +0200

    LectureController implemented.

--- src/main/java/com/project/siiproject/feature/lecture/controller/LectureController.java
+++ src/main/java/com/project/siiproject/feature/lecture/controller/LectureController.java
@@ -1,7 +1,45 @@
 package com.project.siiproject.feature.lecture.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.siiproject.feature.lecture.model.Lecture;
+import com.project.siiproject.feature.lecture.service.LectureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/lectures")
 public class LectureController {
+
+    private final LectureService lectureService;
+
+    @Autowired
+    public LectureController(LectureService lectureService) {
+        this.lectureService = lectureService;
+    }
+
+    @GetMapping
+    public List<Lecture> getAllLectures() {
+        return lectureService.getAllLectures();
+    }
+
+    @GetMapping("{id}")
+    public Lecture getLectureById(@PathVariable final Long id) {
+        return lectureService.getLectureById(id);
+    }
+
+    @PostMapping
+    public Lecture save(@RequestBody final Lecture lecture) {
+        return lectureService.save(lecture);
+    }
+
+    @PutMapping
+    public Lecture update(@RequestBody final Lecture lecture) {
+        return lectureService.save(lecture);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody final Lecture lecture) {
+        lectureService.delete(lecture);
+    }
 }
