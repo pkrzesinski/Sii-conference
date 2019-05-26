f224ac815afc55beddd1be06bc07375c64d43b71
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-26 22:59:36 +0200

    ProjectUI class created for Vaadin.

---
+++ src/main/java/com/project/siiproject/vaadin/ProjectUI.java
@@ -0,0 +1,14 @@
+package com.project.siiproject.vaadin;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class ProjectUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
