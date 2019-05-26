cfd316cf5f098e26d06b27fca0ad8ce609b0003b
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-26 21:10:32 +0200

    UserService implemented.

--- src/main/java/com/project/siiproject/feature/user/dao/UserRepository.java
+++ src/main/java/com/project/siiproject/feature/user/dao/UserRepository.java
@@ -2,6 +2,8 @@ package com.project.siiproject.feature.user.dao;
 
 import com.project.siiproject.feature.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserDao extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/project/siiproject/feature/user/service/UserService.java
+++ src/main/java/com/project/siiproject/feature/user/service/UserService.java
@@ -1,7 +1,37 @@
 package com.project.siiproject.feature.user.service;
 
+import com.project.siiproject.feature.user.dao.UserRepository;
+import com.project.siiproject.feature.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(final Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public User save(final User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(final User user) {
+        userRepository.delete(user);
+    }
 }
