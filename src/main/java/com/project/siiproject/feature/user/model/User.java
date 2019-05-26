275c0cc1b9d761cac177ae7084c69f03c51c63a4
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-26 20:58:25 +0200

    User entity created with many-to-many relation with lecture.

--- src/main/java/com/project/siiproject/feature/user/model/User.java
+++ src/main/java/com/project/siiproject/feature/user/model/User.java
@@ -1,13 +1,31 @@
 package com.project.siiproject.feature.user.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.project.siiproject.feature.lecture.model.Lecture;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 public class User {
 
     @Id
-    @Column(name = "id")
+    @Column(name = "user_id")
     private Long id;
+    @NotEmpty
+    @Column(length = 64)
+    private String name;
+    @NotEmpty
+    @Column(length = 64)
+    private String surname;
+    @Email
+    private String email;
+    @ManyToMany
+    @JoinTable(name = "USERS_TO_LECTURES",
+    joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "lecture_id"))
+    private List<Lecture> lectures;
+
+
 }
